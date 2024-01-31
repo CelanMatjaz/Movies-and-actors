4e3f867a5bdec1d2f5a95c2202c2babf356bd782
CelanMatjaz <matjazcelangithub@gmail.com>
2024-01-31 19:37:37 +0100

    add actors migrations

---
+++ Actors/Migrations/20240131183437_InitialMigration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Actors.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "actors",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    born_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    list_of_movies = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_actors", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "actors");
+        }
+    }
+}
---
+++ Actors/Migrations/20240131183507_InsertDummyData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Actors.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertDummyData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var date = DateOnly.Parse("1-1-2000");
+
+            for (int i = 0; i < insertDataCount; ++i)
+            {
+                migrationBuilder.InsertData(
+                    "actors",
+                    ["first_name", "last_name", "born_date", "list_of_movies"],
+                    [$"Actor {i + 1} first name", $"Actor {i + 1} last name", date.AddDays(i), $"Movies {i + 1}"]
+                );
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            for (int i = 0; i < insertDataCount; ++i)
+            {
+                migrationBuilder.DeleteData("actors", "id", 1);
+            }
+        }
+
+        static uint insertDataCount = 50;
+    }
+}

e1d13b0addbf3d2fa2e4042247eb2746e7f419a6
CelanMatjaz <matjazcelangithub@gmail.com>
2024-01-31 20:48:34 +0100

    add service to docker-compose

--- Actors/Program.cs
+++ Actors/Program.cs
@@ -3,7 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("Database");
+var connectionString = builder.Configuration.GetConnectionString(args.Length == 1 ? args[0] : "Database");
+
 builder.Services.AddDbContext<ActorsDbContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddResponseCaching();
 builder.Services.AddControllers();

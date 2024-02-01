eda793de7e5b959a881d597858db8e0dd558007b
CelanMatjaz <matjazcelangithub@gmail.com>
2024-02-01 21:38:25 +0100

    add request saving to actors service

--- Actors/Program.cs
+++ Actors/Program.cs
@@ -1,4 +1,5 @@
 using ActorsService;
+using CommonData;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Database");
 
 builder.Services.AddDbContext<ActorsDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddDbContext<RequestEntryDbContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddResponseCaching();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseMiddleware<RequestCountMiddleware>("movies");
+
 app.MapControllerRoute("actors", "{controller=Actors}/{action=GetActors}/{id?}");
 app.Run();
